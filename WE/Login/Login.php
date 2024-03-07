ef5fc54ce371f6afd2a4166cb801d3982593767d
Cauã Mahl <cauamahl@yahoo.com.br>
2024-03-07 11:52:09 -0300

    produzindo webservices

--- WE/Login/Login.php
+++ WE/Login/Login.php
@@ -1,24 +1,20 @@
-<?php
 
-$banco = Db::Read()
-->clear()
-->select([
-    "ccr.valor",
-    "ccr.valor_desconto",
-    "ccr.valor_juros",
-    "ccr.valor_multa",
-    "ccr.valor_adesao",
-    "ccr.valor_desconto_manual",
-    "ccr.dt_vencimento_origem",
-    "ccr.valor_reajuste",
-    'bp.cpf'
-])
-->from('gs_contrato gc')
-->leftJoin('base_pessoa bp', 'bp.idpessoa = gc.idpessoa')
-->leftJoin('cx_conta_receber ccr', 'ccr.idcontrato = gc.idcontrato')
-->whereAND([
-    "gc.num_contrato" => $data['contrato'],
-    "ccr.status"      => "AP"
-])
-->orderByASC('ccr.dt_vencimento_origem')
-->fetchall();
+function WebServiceExec($params, $data)
+{
+    $banco = Db::Read()
+        ->clear()
+        ->select(["bp.nome"])
+        ->from('base_pessoa bp')
+        ->whereAND([
+            "bp.email" => $data['email'],
+            "bp.cpf"   => $data['cpf']
+        ])
+        ->fetch();
+
+    if (is_null($banco["nome"])) {
+        return ["logado" => False];
+    }
+
+    $banco["logado"] = True;
+    return $banco;
+}
---
+++ WE/Login/testes.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+
+esilveira@imply.com
+93271269041
+Eduardo Nunes
+
+{
+    "email": "esilveira@imply.com"
+    "cpf" : "93271269041"
+}
+
+_____________________________________________
+
+jefersongvargas2009@hotmail.com
+65948272788
+Jeferson Garcia
+
+{
+    "email": "jefersongvargas2009@hotmail.com"
+    "cpf" : "65948272788"
+}
+
+*/
