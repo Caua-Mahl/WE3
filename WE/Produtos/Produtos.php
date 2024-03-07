12df1f6a0e6dad603fb8698f1151dacf95ceb5d1
Caua-Mahl <cauamahl@yahoo.com.br>
2024-03-07 14:47:33 -0300

    v0.3

--- WE/Produtos/Produtos.php
+++ WE/Produtos/Produtos.php
@@ -1,2 +1,21 @@
 <?php 
-    
+function WebServiceExec($params, $data){
+	$json = Db::Read()->clear()
+		->select([
+			'p.dscproduto',
+			'p.preco',
+			'p.idproduto'
+		])
+		->from ('pdv_produto p')
+        ->whereAND([
+			'p.preco' => new we_Where_NotEqual(0),
+			'p.modalidade' => $data['modalidade'],
+			'p.ativo' => true		   
+		])
+		->limitDB(4)
+	    ->fetchAll();
+
+    return $json;
+}
+	
+	
